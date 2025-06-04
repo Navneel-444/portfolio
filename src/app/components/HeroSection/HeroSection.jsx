935321f646742a226b6c5b22590b54d5dfee455c
Navneel Nandran <navneel.nandran@outlook.com>
2025-06-04 17:36:33 -0400

    chore: scaffold for hero section

---
+++ src/app/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+import HeroStatement from "@/app/ui/HeroStatement/HeroStatement";
+
+export default function HeroSection() {
+    return (
+        <section className="hero-section">
+            <HeroStatement />
+        </section>
+    )
+} 
---
+++ src/app/ui/HeroStatement/HeroStatement.jsx
@@ -0,0 +1,9 @@
+import './HeroStatement.scss';
+
+export default function HeroStatement() {
+    return (
+        <>
+            <h1 className='hero-statement__title'>Hi, I'm Navneel</h1>
+        </>
+    )
+}  
