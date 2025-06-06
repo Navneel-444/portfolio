3fd59ffadcdb0f008235a5e2e672a921ad9e9956
Navneel Nandran <navneel.nandran@outlook.com>
2025-06-06 15:47:19 -0400

    feat: add reusable section heading ui

---
+++ src/app/components/ProjectsSection/ProjectSection.jsx
@@ -0,0 +1,8 @@
+import SectionHeading from '@/app/ui/SectionHeading/SectionHeading';
+import './ProjectSection.scss';
+
+export default function ProjectSection() {
+    return (
+        <SectionHeading heading="Projects" />
+    )
+}
