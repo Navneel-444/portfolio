15162f86f9d1e79aa0e22d36140dbc08cb527e0b
Navneel Nandran <navneel.nandran@outlook.com>
2025-06-17 15:44:31 -0400

    feat: add hover effect and open link in new tab

--- src/app/ui/ContactInfo/ContactInfo.jsx
+++ src/app/ui/ContactInfo/ContactInfo.jsx
@@ -5,9 +5,9 @@ import GithubIcon from '../../../assets/icons/github-icon.svg';
 
 export default function ContactInfo() {
     const socials = [
-        { address: "navneel.nandran@gmail.com", icon: MailIcon, url: "mailto:you@example.com" },
-        { address: "navneel-444", icon: GithubIcon, url: "https://github.com/navneel-444" },
-        { address: "navneel-nandran", icon: LinkedInIcon, url: "https://linkedin.com/in/navneel-nandran" }
+        { label: "navneel.nandran@gmail.com", icon: MailIcon, url: "mailto:navneel.nandran@gmail.com" },
+        { label: "navneel-444", icon: GithubIcon, url: "https://github.com/navneel-444" },
+        { label: "navneel-nandran", icon: LinkedInIcon, url: "https://linkedin.com/in/navneel-nandran" }
     ]
     return (
         <section className="contact-info">
@@ -19,9 +19,9 @@ export default function ContactInfo() {
             </div>
             <div className="contact-info__social">
                 {socials.map((link, idx) => (
-                    <a href={link.url} key={idx} className="contact-info__social-link">
+                    <a href={link.url} target="_blank" rel="noopener noreferrer" key={idx} className="contact-info__social-link">
                         <link.icon className="contact-info__social-icon" />
-                        <p className="contact-info__social-name">{link.address}</p>
+                        <p className="contact-info__social-name">{link.label}</p>
                     </a>
                 ))}
             </div>
