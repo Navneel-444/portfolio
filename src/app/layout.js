cfe921f240d79464db4dce716972d6b43c4a1df9
Navneel Nandran <navneel.nandran@outlook.com>
2025-06-20 00:55:36 -0400

    feat: add jetbrains font using next font module'

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,15 @@
 import '@/styles/main.scss';
 import Navigation from '@/app/ui/navigation/Navigation.jsx';
+import { JetBrains_Mono } from 'next/font/google'
+
+const jetbrains = JetBrains_Mono({
+  subsets: ['latin'],
+  fallback: ['Consolas', 'monospace', 'Source Code Pro', 'Menlo']
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={jetbrains.className}>
       <body>
         <Navigation />
         {children}
