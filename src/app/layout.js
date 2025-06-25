42ae8b37f682ed8a597778d960e75f7cea50afba
Navneel Nandran <navneel.nandran@outlook.com>
2025-06-24 23:16:33 -0400

    feat: add mobile navigation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+'use client'
 import '@/styles/main.scss';
 import Navigation from '@/app/ui/navigation/Navigation.jsx';
+import MobileNav from './ui/MobileNav/MobileNav';
 import { JetBrains_Mono } from 'next/font/google'
+import { useState, useEffect } from 'react';
 
 const jetbrains = JetBrains_Mono({
   subsets: ['latin'],
@@ -8,10 +11,24 @@ const jetbrains = JetBrains_Mono({
 })
 
 export default function RootLayout({ children }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const windowResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    windowResize();
+    window.addEventListener('resize', windowResize);
+
+    return () => {
+      window.removeEventListener('resize', windowResize);
+    };
+  }, []);
   return (
     <html lang="en" className={jetbrains.className}>
       <body>
-        <Navigation />
+        {isMobile ? <MobileNav /> : <Navigation />}
         {children}
       </body>
     </html>
