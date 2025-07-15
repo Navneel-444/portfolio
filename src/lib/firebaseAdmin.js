9cecdf64b6c427461baa086c8a05f89612e24f0f
Navneel Nandran <navneel.nandran@outlook.com>
2025-07-15 01:20:39 -0400

    fix: missing import

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,3 +1,6 @@
+import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
