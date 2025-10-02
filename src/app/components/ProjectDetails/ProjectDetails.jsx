58a3fce42e71f695026824c51d2e049017c490e9
Navneel Nandran <navneel.nandran@outlook.com>
2025-10-02 15:41:29 -0400

    feat: display simple text for project description'

--- src/app/components/ProjectDetails/ProjectDetails.jsx
+++ src/app/components/ProjectDetails/ProjectDetails.jsx
@@ -1,8 +1,37 @@
+import '../../../styles/main.scss';
+
 export default function ProjectDetails({ project }) {
+    const {
+        desc,
+        overview,
+        'problem statement': problemStatement,
+        'key features': keyFeatures,
+        'tech stack': techStack,
+        'what i learned ': whatILearned,
+        'architecture / system design': architecture,
+        'future improvements': futureImprovements,
+        name
+    } = project;
+
     return (
-        <div>
-            <h1>{project?.name ?? 'Unnamed project'}</h1>
-            <p>{project?.desc ?? 'No description'}</p>
-        </div>
+        <main>
+            <h1>{name}</h1>
+            <h2>Descritption</h2>
+            <p>{desc}</p>
+            <h2>What I learned </h2>
+            <p>{whatILearned}</p>
+            <h2>Architecture/System Design</h2>
+            <p>{architecture}</p>
+            <h2>Future Improvments</h2>
+            <p>{futureImprovements}</p>
+            <h2>Key Features</h2>
+            <p>{keyFeatures}</p>
+            <h2>Problem Statement</h2>
+            <p>{problemStatement}</p>
+            <h2>Tech Stack</h2>
+            <p>{techStack}</p>
+            <h2>Overview</h2>
+            <p>{overview}</p>
+        </main>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import ContactSection from './components/ContactSection/ContactSection';
 
 export default function Home() {
   return (
-    <main className='main'>
+    <main>
       <HeroSection />
       <ProjectSection />
       <ExperienceSection />
