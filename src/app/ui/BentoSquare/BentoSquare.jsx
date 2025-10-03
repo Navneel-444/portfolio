f77f1080ce4d7011662a108dc8f86c965d65b50e
Navneel Nandran <navneel.nandran@outlook.com>
2025-10-03 00:14:01 -0400

    chore: fixed BEM naming for sqaure bento box

--- src/app/ui/BentoSquare/BentoSquare.jsx
+++ src/app/ui/BentoSquare/BentoSquare.jsx
@@ -4,15 +4,15 @@ export default function BentoSquare({ heading, info }) {
 
     return (
         <section className="square">
-            <h2 className="square-title">{heading}</h2>
+            <h2 className="square__title">{heading}</h2>
             {Array.isArray(info) ? (
-                <ul className="square-info">
+                <ul className="square__info">
                     {info.map((item, index) => (
                         <li key={index}>{item}</li>
                     ))}
                 </ul>
             ) : (
-                <p className="square-info">{info}</p>
+                <p className="square__info">{info}</p>
             )}
         </section>
     )
