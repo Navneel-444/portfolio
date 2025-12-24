c064396ebba41d7af11a332a5acf501ae81692ca
Navneel Nandran <navneel.nandran@outlook.com>
2025-12-23 21:29:43 -0500

    feat: add render for object values for project detail page"

--- src/app/ui/BentoItem/BentoItem.jsx
+++ src/app/ui/BentoItem/BentoItem.jsx
@@ -16,6 +16,8 @@ export default function BentoItem({ heading, info, variant, imageUrl, index }) {
 
     const hasContent = info && (Array.isArray(info) ? info.length > 0 : true);
 
+    const isArrayOfObjects = Array.isArray(info) && info.length > 0 && typeof info[0] === 'object' && info[0] !== null;
+
     return (
         <section
             className={`bento-box__item ${variantClasses[variant] || ''}`}
@@ -24,7 +26,21 @@ export default function BentoItem({ heading, info, variant, imageUrl, index }) {
             <h2 className="bento-box__title">{heading || 'No content available'}</h2>
             <BentoItemModal heading={heading} info={info}>
                 {hasContent ? (
-                    Array.isArray(info) ? (
+                    isArrayOfObjects ? (
+                        <ul className="bento-box__info">
+                            {info.map((item, index) => (
+                                <h4 key={index}>
+                                    {Object.entries(item).map(([key, value], i) => (
+                                        <div key={i}>
+                                            <strong>{key}</strong> <br />
+                                            <p className="bento-box__subheading">{value}</p>
+                                            <br />
+                                        </div>
+                                    ))}
+                                </h4>
+                            ))}
+                        </ul>
+                    ) : Array.isArray(info) ? (
                         <ul className="bento-box__info">
                             {info.map((item, index) => (
                                 <li key={index}>{item}</li>
@@ -37,6 +53,6 @@ export default function BentoItem({ heading, info, variant, imageUrl, index }) {
                     <p className="bento-box__info bento-box__no-content">No content available</p>
                 )}
             </BentoItemModal>
-        </section>
+        </section >
     );
 }
