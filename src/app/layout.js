a915bf36375344f2724a68d5b817a024af4ec57b
Navneel Nandran <navneel.nandran@outlook.com>
2026-01-05 21:14:28 -0500

    bug: the page was not redirect to the section on the home page from the project detail page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import '@/styles/main.scss';
 import Navigation from '@/app/ui/navigation/Navigation.jsx';
 import MobileNav from './ui/MobileNav/MobileNav';
 import PageTimeTracker from './ui/PageTimeTracker/PageTimeTracker';
-import HashScrollHandler from './ui/HashScrollHandler/HashScrollHandler';
 import { JetBrains_Mono } from 'next/font/google'
 
 const jetbrains = JetBrains_Mono({
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className={jetbrains.className}>
       <body>
         <PageTimeTracker>
-          <HashScrollHandler />
           <Navigation />
           <MobileNav />
           {children}
--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import HeroSection from '@/app/components/HeroSection/HeroSection';
 import ProjectSection from '@/app/components/ProjectSection/ProjectSection';
 import ExperienceSection from './components/ExperienceSection/ExperienceSection';
 import ContactSection from './components/ContactSection/ContactSection';
+import HashScrollHandler from './ui/HashScrollHandler/HashScrollHandler';
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      <ProjectSection />
-      <ExperienceSection />
-      <ContactSection />
-    </main>
+    <>
+      <HashScrollHandler />
+      <main>
+        <HeroSection />
+        <ProjectSection />
+        <ExperienceSection />
+        <ContactSection />
+      </main>
+    </>
   );
 }
